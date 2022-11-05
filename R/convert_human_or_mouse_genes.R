a9f50228efb903bdc7bcb56d74004c46341858d0
Rebecca <rebecca.jaszczak@ucsf.edu>
2022-11-04 17:14:16 -0700

    add host validation & refresh option

--- R/convert_human_or_mouse_genes.R
+++ R/convert_human_or_mouse_genes.R
@@ -9,7 +9,7 @@
 #' @keywords genes convert
 #' @export
 #' @examples
-#' convert_human_or_mouse_genes("DPPA3", host = 'www.ensembl.org')
+#' convert_human_or_mouse_genes("DPPA3", host = 'https://www.ensembl.org')
 #' convert_human_or_mouse_genes("DPPA3", unique = TRUE)
 #' convert_human_or_mouse_genes("DPPA3")
 #' convert_human_or_mouse_genes("Dppa3", unique = TRUE)
@@ -17,68 +17,67 @@
 
 
 convert_human_or_mouse_genes = function (genelist, unique = FALSE, host = "https://dec2021.archive.ensembl.org") {
-  if ( !is.null(genelist) ) stop("Pass a mouse or human gene or vector of genes to convert.")
-
-  #tests if the characters in the vector are in all caps
+  if (is.null(genelist))
+    stop("Pass a mouse or human gene or vector of genes to convert")
   tf = all(grepl("^[[:upper:]]+$", gsub("[[:digit:]]+", "",
                                         genelist)))
-
-  #based on boolean tf, assigns from to "human" or "mouse"
   from = "var"
   if (isTRUE(tf)) {
     assign(x = "from", value = "human")
-    print("from assigned to human")
+    print("A list of human genes will be converted to mouse")
   }
   else if (!isTRUE(tf)) {
     assign(x = from, value = "mouse")
-    print("from assigned to mouse")
+    print("A list of mouse genes will be converted to human")
   }
-
-  #loads in biomarts if they haven't been loaded already
-  if (!exists("human_mart")) {
-    if ( isTRUE(grepl(host, human_mart@host)) ) {
+  if ( exists("human_mart") ) {
+    mart_id = sub("org:","org",sub("443.*","", human_mart@host))
+    if ( mart_id == host) {
       print("marts already exist in .GlobalEnv, starting conversion")
-    } else {
-      require("biomaRt")
-      print(paste0("host is: ",host," - loading now"))
-      human_mart = useEnsembl(
-        "ensembl",
-        dataset = "hsapiens_gene_ensembl",
-        host = host)
-      assign(x = "human_mart", value = human_mart, .GlobalEnv)
-      if (!exists("mouse_mart")) {
-        mouse_mart = useEnsembl("ensembl",
-                                dataset = "mmusculus_gene_ensembl",
-                                host = host)
-        assign(x = "mouse_mart", value = mouse_mart, .GlobalEnv)
-      }
     }
+    else {
+      print("marts in .GlobalEnv don't match requested host; refreshing...")
+      require("biomaRt")
+      print(paste0("host is: ", host, " - loading now"))
+      human_mart = useEnsembl("ensembl", dataset = "hsapiens_gene_ensembl",
+                              host = host)
+      assign(x = "human_mart", value = human_mart, .GlobalEnv)
+      mouse_mart = useEnsembl("ensembl", dataset = "mmusculus_gene_ensembl",
+                              host = host)
+      assign(x = "mouse_mart", value = mouse_mart,
+             .GlobalEnv)
+    }
+  } else {
+    require("biomaRt")
+    print(paste0("host is: ", host, " - loading now"))
+    human_mart = useEnsembl("ensembl", dataset = "hsapiens_gene_ensembl",
+                            host = host)
+    assign(x = "human_mart", value = human_mart, .GlobalEnv)
+    mouse_mart = useEnsembl("ensembl", dataset = "mmusculus_gene_ensembl",
+                            host = host)
+    assign(x = "mouse_mart", value = mouse_mart,
+           .GlobalEnv)
   }
-  else {
-    print("marts already exist in .GlobalEnv, starting conversion")
-  }
-
-  #convert from human to mouse
   if (from == "human") {
     print("converting from human to mouse")
-    ens = getBM(attributes = c("hgnc_symbol","ensembl_gene_id"),
-                mart = human_mart, values = genelist, filters = "hgnc_symbol", uniqueRows = F)
-    genesV2 = getLDS(attributes = c("hgnc_symbol","ensembl_gene_id"), filters = c("ensembl_gene_id"),
-                     mart = human_mart, values = ens$ensembl_gene_id, attributesL = c("mgi_symbol", "ensembl_gene_id"),
-                     martL = mouse_mart, uniqueRows = F)
+    ens = getBM(attributes = c("hgnc_symbol", "ensembl_gene_id"),
+                mart = human_mart, values = genelist, filters = "hgnc_symbol",
+                uniqueRows = F)
+    genesV2 = getLDS(attributes = c("hgnc_symbol", "ensembl_gene_id"),
+                     filters = c("ensembl_gene_id"), mart = human_mart,
+                     values = ens$ensembl_gene_id, attributesL = c("mgi_symbol",
+                                                                   "ensembl_gene_id"), martL = mouse_mart, uniqueRows = F)
   }
   else {
-    #convert from mouse to human
     print("converting from mouse to human")
-    ens = getBM(attributes = c("mgi_symbol","ensembl_gene_id"),
-                mart = mouse_mart, values = genelist, filters = "mgi_symbol", uniqueRows = F)
-    genesV2 = getLDS(attributes = c("mgi_symbol","ensembl_gene_id"), filters = c("ensembl_gene_id"),
-                     mart = mouse_mart, values = ens$ensembl_gene_id, attributesL = c("hgnc_symbol", "ensembl_gene_id"),
-                     martL = human_mart,
-                     uniqueRows = F)
+    ens = getBM(attributes = c("mgi_symbol", "ensembl_gene_id"),
+                mart = mouse_mart, values = genelist, filters = "mgi_symbol",
+                uniqueRows = F)
+    genesV2 = getLDS(attributes = c("mgi_symbol", "ensembl_gene_id"),
+                     filters = c("ensembl_gene_id"), mart = mouse_mart,
+                     values = ens$ensembl_gene_id, attributesL = c("hgnc_symbol",
+                                                                   "ensembl_gene_id"), martL = human_mart, uniqueRows = F)
   }
-
-  # an in R calculation of unique values in the genesV2 dataframe; different than the biomart serverside uniqueRows calculation
   if (isTRUE(unique)) {
     genesV2_unique = unique(genesV2[, 2])
     print(head(genesV2_unique))
